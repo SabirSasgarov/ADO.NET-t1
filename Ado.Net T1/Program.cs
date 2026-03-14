e04b28f3cc68a2de3afb8a56ee862e4c4684dccf
SabirA <sabirsas@code.edu.az>
2026-03-14 17:32:02 +0400

    fisrt look

---
+++ Ado.Net T1/Program.cs
@@ -0,0 +1,55 @@
+﻿using Azure.Core;
+using Microsoft.Data.SqlClient;
+using System;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Ado.Net_T1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+			foreach (Student student in GetAllStudents())
+			{
+                Console.WriteLine(student);
+			}
+		}
+
+        static SqlConnection Connection()
+        {
+            string connStr = "Server=SABIR\\MSSQLSERVER01;Database=Students;Trusted_Connection=True; Trust Server Certificate = True;";
+			SqlConnection conn = new SqlConnection(connStr);
+            conn.Open();
+			Console.WriteLine("Connection successful");
+            return conn;
+        }
+
+        static List<Student> GetAllStudents()
+        {
+            List<Student> students = new List<Student>();
+            using SqlConnection conn = Connection();
+            string query = "SELECT * FROM StudentsTB";
+            SqlCommand cmd = new SqlCommand(query,conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                if (reader.HasRows)
+                {
+                    int id = Convert.ToInt32(reader["Id"]);
+                    string name = Convert.ToString(reader["Name"]);
+                    int age = Convert.ToInt32(reader["Age"]);
+                    Student newStudent = new Student(id, name, age);
+                    students.Add(newStudent);
+                }
+            }
+            return students;
+        }
+
+
+
+
+
+    }
+}
---
+++ SQLQuery1.sql
@@ -0,0 +1,21 @@
+﻿CREATE DATABASE Students
+
+USE Students
+
+CREATE TABLE StudentsTB
+(
+	Id INT PRIMARY KEY IDENTITY,
+	[Name] NVARCHAR(20) NOT NULL,
+	Age INT CHECK(Age>0)
+)
+
+INSERT INTO StudentsTB VALUES
+('Sabir',22),
+('Xatire',25),
+('MirHuseyn',24),
+('Elcin',21),
+('Ferid',18),
+('Miryam',16);
+
+SELECT * FROM StudentsTB
+
