443e630a4a832f534580f2942b97995545562a49
SabirA <sabirsas@code.edu.az>
2026-03-15 12:39:23 +0400

    added some methods

--- Ado.Net T1/Program.cs
+++ Ado.Net T1/Program.cs
@@ -6,50 +6,90 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Ado.Net_T1
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-			foreach (Student student in GetAllStudents())
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			while (true)
 			{
-                Console.WriteLine(student);
+				Console.WriteLine("1 - Get all\n2 - Add\n0 - Quit");
+				string operation = Console.ReadLine();
+				switch (operation)
+				{
+					case "1":
+						foreach (Student student in GetAllStudents())
+							Console.WriteLine(student);
+						break;
+					
+					case "2":
+						Console.WriteLine("Enter name and age");
+						string name = Console.ReadLine();
+						int age = int.Parse(Console.ReadLine());
+						AddStudent(name, age);
+						Console.Clear();
+						Console.WriteLine("Added successfully!");
+						break;
+					
+					case "0":
+						Console.WriteLine("---Exit---");
+						return;
+					
+					default:
+						Console.WriteLine("Wrong operation!");
+						break;
+				}
 			}
+			//task2
+
+
+
 		}
 
-        static SqlConnection Connection()
-        {
-            string connStr = "Server=SABIR\\MSSQLSERVER01;Database=Students;Trusted_Connection=True; Trust Server Certificate = True;";
+		static SqlConnection Connection()
+		{
+			string connStr = "Server=SABIR\\MSSQLSERVER01;Database=Students;Trusted_Connection=True; Trust Server Certificate = True;";
 			SqlConnection conn = new SqlConnection(connStr);
-            conn.Open();
+			conn.Open();
 			Console.WriteLine("Connection successful");
-            return conn;
-        }
+			return conn;
+		}
 
-        static List<Student> GetAllStudents()
-        {
-            List<Student> students = new List<Student>();
-            using SqlConnection conn = Connection();
-            string query = "SELECT * FROM StudentsTB";
-            SqlCommand cmd = new SqlCommand(query,conn);
-            SqlDataReader reader = cmd.ExecuteReader();
-            while (reader.Read())
-            {
-                if (reader.HasRows)
-                {
-                    int id = Convert.ToInt32(reader["Id"]);
-                    string name = Convert.ToString(reader["Name"]);
-                    int age = Convert.ToInt32(reader["Age"]);
-                    Student newStudent = new Student(id, name, age);
-                    students.Add(newStudent);
-                }
-            }
-            return students;
-        }
+		static List<Student> GetAllStudents()
+		{
+			List<Student> students = new List<Student>();
+			using SqlConnection conn = Connection();
+			string query = "SELECT * FROM StudentsTB";
+			SqlCommand cmd = new SqlCommand(query, conn);
+			SqlDataReader reader = cmd.ExecuteReader();
+			while (reader.Read())
+			{
+				if (reader.HasRows)
+				{
+					int id = Convert.ToInt32(reader["Id"]);
+					string name = Convert.ToString(reader["Name"]);
+					int age = Convert.ToInt32(reader["Age"]);
+					Student newStudent = new Student(id, name, age);
+					students.Add(newStudent);
+				}
+			}
+			return students;
+		}
+
+		static void AddStudent(string name, int age)
+		{
+			using SqlConnection conn = Connection();
+			string query = "INSERT INTO StudentsTB (Name,Age) VALUES (@name, @age)";
+			SqlCommand cmd = new SqlCommand(query, conn);
+			cmd.Parameters.AddWithValue("@name", name);
+			cmd.Parameters.AddWithValue("@age", age);
+			cmd.ExecuteNonQuery();
+		}
 
 
 
 
 
-    }
+
+
+	}
 }
